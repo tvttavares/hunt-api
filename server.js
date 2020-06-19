4b91c85dd362d53cb000555deee32ff43726d069
Thiago Tavares <tvttavares@gmail.com>
2020-06-19 16:10:55 -0400

    Criando o model de Produto

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/hunt-api', { useNewUrlParser: true} );
+mongoose.connect(
+    'mongodb://localhost:27017/huntapi', 
+    { useNewUrlParser: true} 
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'https://github.com/facebook/react-native'
+    })
+    
+    return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);
