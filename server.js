acc7dcd3ac83dab91d0d78d19762172777acf9c7
Thiago Tavares <tvttavares@gmail.com>
2020-06-19 16:30:15 -0400

    Restruturação de arquivos

--- server.js
+++ server.js
@@ -12,17 +12,9 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+//const Product = mongoose.model('Product');
 
-// Primeira rota
-app.get('/', (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'https://github.com/facebook/react-native'
-    })
-    
-    return res.send('Hello Rocketseat');
-});
+// Rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
