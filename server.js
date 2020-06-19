7cd5f42e69969515c767dc4b00840f78b53dbdea
Thiago Tavares <tvttavares@gmail.com>
2020-06-19 15:49:13 -0400

    Instalando e conectando com o Database MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/hunt-api', { useNewUrlParser: true} );
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello Rocketseat');
 });
